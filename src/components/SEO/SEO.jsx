70b4e9c51f5de74e527a9b7fd9bab26c50830415
logan-r <logan.ralston@mail.mcgill.ca>
2021-01-30 10:24:28 -0500

    feat: meta image

--- src/components/SEO/SEO.jsx
+++ src/components/SEO/SEO.jsx
@@ -2,7 +2,9 @@ import React from "react"
 import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 
-const SEO = ({ description, title, lang }) => (
+import bannerImage from "../../assets/images/banner.png"
+
+const SEO = ({ description, title, lang, image }) => (
   <Helmet>
     <html lang={lang} />
     <title lang={lang}>{title}</title>
@@ -11,6 +13,7 @@ const SEO = ({ description, title, lang }) => (
     <meta name="robots" content="index, follow" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="theme-color" content="#ffffff"></meta>
+    <meta name="image" content={bannerImage} />
   </Helmet>
 )
 
@@ -19,14 +22,14 @@ SEO.defaultProps = {
   meta: ["hackathon", "Canada", "McGill", "Mentorship"],
   description:
     "Canada’s original university hackathon. 36 hour long event of innovation, coding & mentorship happening January 29 - 31 at McGill University",
-  title: "McHacks",
+  title: "McHacks"
 }
 
 SEO.propTypes = {
   description: PropTypes.string,
   lang: PropTypes.string,
   meta: PropTypes.arrayOf(PropTypes.object),
-  title: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired
 }
 
 export default SEO
